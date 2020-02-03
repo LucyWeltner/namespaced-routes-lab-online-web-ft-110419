436e08e76b566f3bca26a27237b4e2d78f3006ab
LucyWeltner <lucyiweltner@gmail.com>
2020-02-03 18:06:19 -0500

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,25 @@
+class Preference < ActiveRecord::Base
+	# def initialize(attr_hash)
+	# 	@allow_create_artists = attr_hash[:allow_create_artists]
+	# 	@allow_create_songs = attr_hash[:allow_create_songs]
+	# end
+
+	# def allow_create_artists
+	# 	@allow_create_artists
+	# end 
+	# def allow_create_artists=(boolean)
+	# 	@allow_create_artists = boolean
+	# end 
+	# def allow_create_songs=(boolean)
+	# 	@allow_create_songs = boolean
+	# end
+	# def allow_create_songs
+	# 	@allow_create_songs
+	# end 
+
+end
+
+# p = Preference.new
+
+# p.allow_create_artists = true
+# puts p.allow_create_artists
